f1e6664a3da1bde36b5fdc246836f4a7a16be536
nate <nathanqthai@users.noreply.github.com>
2022-01-02 06:06:58 -0500

    Added check for Opus when using voice.

--- bot.py
+++ bot.py
@@ -137,6 +137,10 @@ class Taunt(commands.Cog):
         self._manifest = json.load(self._do_api.get_object(self._space, manifest))
         self._taunt_min, self._taunt_max = self.get_taunt_range()
 
+        discord.opus.load_opus()
+        if not discord.opus.is_loaded():
+            raise Exception('Opus failed to load')
+
         self.log.info(f"Registered {self.__class__.__name__} cog to {bot_name}")
 
     def get_taunt_range(self) -> Tuple[int, int]:
